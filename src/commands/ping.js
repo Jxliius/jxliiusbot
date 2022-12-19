e30ceb07e46a30a6308ce28b6568fd731267227f
Jxliius <julius.reiter001@gmail.com>
2022-12-19 18:38:00 +0100

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,7 +4,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder().setName("ping").setDescription("Pong!"),
     async execute(interaction){
-        interaction.reply(`Pong 🏓`)
+        //interaction.reply(`Pong 🏓`)
+        interaction.reply(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
     }
 }
 
